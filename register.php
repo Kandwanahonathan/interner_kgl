700658d3dc4338f6ee6e93fef74d285f5dc79f03
NATHAN Akandwanaho <nathankandwanaho3@gmail.com>
2025-04-17 12:21:49 +0200

    mi session

---
+++ register.php
@@ -0,0 +1,26 @@
+<html lang="en"><head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login for doctor</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+    <div class="form-group col-md-5">
+        <form action="" method="post">
+            <h1>welcome doctor</h1>
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" name="name">
+                <label for="">Name</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="password" class="form-control" name="password">
+                <label for="">Password</label>
+            </div>
+            <button class="btn btn-warning form-control" name="submit">submit</button>
+        </form>
+    </div>
+   
+    
+</body>
+</html>
+<?php
